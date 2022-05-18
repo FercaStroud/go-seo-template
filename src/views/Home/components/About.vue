10d4752a44a7c3b5ea13c0833d5ecc2a620056a3
Reli <naitvenefy@gmail.com>
2022-05-18 11:41:48 -0500

    center services

--- src/views/Home/components/About.vue
+++ src/views/Home/components/About.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-.features-container.container.mx-auto.mt-10.mb-0(class="sm:mb-10")
-  .about.text-center.flex.flex-col.justify-center.items-center.p-5(class="sm:p-10")
+.container.mx-auto.my-5
+  .about.flex.flex-col.justify-center.items-center.text-center.p-5(class="sm:p-10")
     .ql-editor(v-html="$store.state.settings.about" )
 
 </template>
--- src/views/Home/components/Services.vue
+++ src/views/Home/components/Services.vue
@@ -1,21 +1,18 @@
 <template lang="pug">
-.features-container.container.mx-auto
-  .about.text-center.flex.flex-col.justify-center.items-center.gap-5.p-10.mt-10(class='sm:px-20')
-    .features.flex.flex-row.flex-wrap.justify-center.my-10
-      .feature.p-5.flex-col.text-center(
-        v-for="(service, key) in $store.state.services", :key="key"
-        class='basis-1/2 sm:basis-1/3 md:p-10'
+.container.mx-auto.my-5
+  .flex.flex-col.justify-center.items-center.text-center
+    .features.flex.flex-wrap.justify-center.items-center
+      .flex.flex-col.text-center.p-5.basis-full(
+        class="basis-1/2 sm:basis-1/3 md:p-10",
+        v-for="(service, key) in $store.state.services",
+        :key="key"
       )
-        p.text-xl.mb-5(class='sm:text-2xl lg:text-4xl')
+        .flex.justify-center.items-center.p-2.mb-5.h-10
           img(
-            style="float:left;width:20px; margin-right:5px"
-            :src='PUBLIC_ASSETS + "images/services/" + service.icon'
-            :alt='service.title'
+            :src="PUBLIC_ASSETS + 'images/services/' + service.icon",
+            :alt="service.title"
           )
-        p.font-black.mb-2(class='sm:text-lg md:text-xl lg:text-2xl')
-          | {{service.title}}
-
-
+        p.font-black.text-base.mb-2(class="sm:text-lg md:text-xl lg:text-2xl") {{ service.title }}
 </template>
 
 <script>
