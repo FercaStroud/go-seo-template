9502bc89b7dda5bee1f4809b185c8bd874d9fd1a
Reli <naitvenefy@gmail.com>
2022-06-22 12:20:45 -0500

    update blog preview on home

--- src/views/Home/components/Blog.vue
+++ src/views/Home/components/Blog.vue
@@ -10,13 +10,13 @@
         template(v-if="key < 3")
           .basis-full.p-2(class="basis-1/3")
             .border.p-5
-              .img-container.flex.justify-center.items-center.mb-5
+              .img-container.flex.justify-center.items-center.mb-2(class="sm:mb-5")
                 img.w-full.h-auto(
                   :src="post.image_src ? PUBLIC_ASSETS + 'images/posts/' + post.image_src : 'https://via.placeholder.com/300x300'",
                   alt=""
                 )
-              .flex.justify-center.items-center.h-20.mb-2
-                h3.title-primary.font-black.text-xl(class="sm:text-2xl") {{ post.title }}
+              .flex.justify-center.items-center.h-20.mb-5
+                h3.title-primary-sm.font-black.text-md(class="sm:text-xl") {{ post.title.length >= 45 ? post.title.substring(0, 42) + '...' : post.title }}
               .h-48.overflow-hidden.mb-5
                 .ql-editor(v-html="post.description")
               .flex.justify-center.items-center
